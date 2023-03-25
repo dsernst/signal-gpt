65f46d0801595a0b70f31d1f85bab8c446ff5156
David Ernst <git@dsernst.com>
2023-03-25 02:16:39 -0500

    Get basic commands working: /beep, /echo, & 'unknown'

---
+++ commands/echo.ts
@@ -0,0 +1,1 @@
+export default (message: string) => message.slice(6)
--- index.ts
+++ index.ts
@@ -1,25 +1,65 @@
 import 'signal-http'
+import fs from 'fs'
 import axios from 'axios'
 import fastify from 'fastify'
 
 const app = fastify()
 
+type Message = {
+  sourceNumber: string
+  sourceName: string
+  message: string
+  groupId?: string
+}
+
 app.post('/message', (req, res) => {
-  // console.log('/message:', JSON.stringify(req.body))
-  const { message, sourceNumber, groupId } = req.body as {
-    sourceNumber: string
-    sourceName: string
-    message: string
-    groupId?: string
+  function respond(message: string, sourceNumber: string, groupId?: string) {
+    axios.post('http://localhost:9460/send', {
+      to: sourceNumber,
+      toGroup: groupId,
+      message,
+    })
+    res.send('OK')
   }
-  axios.post('http://localhost:9460/send', {
-    to: sourceNumber,
-    toGroup: groupId,
-    message,
+
+  // Parse the incoming messages
+  const { message, sourceNumber, groupId } = req.body as Message
+
+  // Check if message matches one of the available commands
+  const commands = getCommands()
+  let match = false
+  commands.forEach((command) => {
+    if (message.startsWith(command)) {
+      match = true
+
+      // Run the matching command
+      import(`./commands/${command.slice(1)}`).then((module) =>
+        respond(
+          module.default(message, sourceNumber, groupId) as string,
+          sourceNumber,
+          groupId
+        )
+      )
+    }
   })
-  res.send('OK')
+  if (match) return
+
+  // Was this message not targeted at our bot?
+  if (groupId && !message.startsWith('/')) return
+
+  // Unknown command
+  const msg = `Unknown command. Available options: ${commands.join(', ')}`
+  respond(msg, sourceNumber, groupId)
 })
 
 app.listen({ port: 9461 }, () => {
   console.log('🟢 Signal-gpt server listening on port 9461')
 })
+
+function getCommands(): string[] {
+  const path = './commands/'
+  const files = fs.readdirSync(path)
+  return files
+    .filter((file) => fs.statSync(path + file).isFile())
+    .map((f) => `/${f.slice(0, -3)}`)
+}
