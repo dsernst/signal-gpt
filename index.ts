ae73002505ca90b11b247e8364794848ac547d39
David Ernst <git@dsernst.com>
2023-03-24 23:17:42 -0500

    Begin hooking up webhook & response to signal-http

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 import 'signal-http'
+import axios from 'axios'
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.post('/message', (req, res) => {
+  //   console.log('/message:', JSON.stringify(req.body))
+  const { fromPhone, message } = req.body as {
+    fromPhone: string
+    message: string
+  }
+  axios.post('http://localhost:9460/send', {
+    to: fromPhone,
+    message,
+  })
+  res.send('OK')
+})
+
+app.listen({ port: 9461 }, () => {
+  console.log('🟢 Signal-gpt server listening on port 9461')
+})
